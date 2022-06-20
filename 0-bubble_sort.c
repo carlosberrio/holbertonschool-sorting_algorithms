2872b1d25f55aa6587219d910203f53f7654caba
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-20 07:39:53 -0700

    Betty style for 0-bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,28 +6,26 @@
  * @size: size of the lengt areay
  * Return: void
  */
-
 void bubble_sort(int *array, size_t size)
 {
-    int ordered = 1;
-    size_t k;
+	int ordered = 1;
+	size_t k;
 
-    if (size < 2)
-        return;
-
-    while (ordered == 1)
-    {
-        ordered = 0;
-        for (k = 0; k < size - 1; k++)
-        {
-            if (array[k] > array[k + 1])
-            {
-                array[k] += array[k + 1];
-                array[k + 1] = array[k] - array[k + 1];
-                array[k] -= array[k + 1];
-                print_array(array, size);
-                ordered = 1;
-            }
-        }
-    }
+	if (size < 2)
+		return;
+	while (ordered == 1)
+	{
+		ordered = 0;
+		for (k = 0; k < size - 1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				array[k] += array[k + 1];
+				array[k + 1] = array[k] - array[k + 1];
+				array[k] -= array[k + 1];
+				print_array(array, size);
+				ordered = 1;
+			}
+		}
+	}
 }
