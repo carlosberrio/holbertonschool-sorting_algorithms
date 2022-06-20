589f9a096892eae91a3c28fd672936dcb6d7552e
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-20 08:02:14 -0700

    Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,33 +8,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *list1= NULL;
-    listint_t *temporal = NULL;
+	listint_t *list1 = NULL;
+	listint_t *temporal = NULL;
 
-    if ((*list)->next == NULL || list == NULL || (*list) == NULL)
-            return;
+	if ((*list)->next == NULL || list == NULL || (*list) == NULL)
+		return;
 
-    list1 = *list;
-    temporal = *list;
+	list1 = *list;
+	temporal = *list;
 
-    while (temporal != NULL)
-    {
-        temporal = temporal->next;
-        while (list1->prev && (list1->n < list1->prev->n))
-        {
-            list1->prev->next = list1->next;
-            if (list1->next != NULL)
-                    list1->next->prev = list1->prev;
-
-            list1->next = list1->prev;
-            list1->prev = list1->next->prev;
-            if (list1->prev != NULL)
-                    list1->prev->next = list1;
-            else
-                    *list = list1;
-            list1->next->prev = list1;
-            print_list(*list);
-        }
-        list1 = temporal;
-    }
+	while (temporal != NULL)
+	{
+		temporal = temporal->next;
+		while (list1->prev && (list1->n < list1->prev->n))
+		{
+			list1->prev->next = list1->next;
+			if (list1->next != NULL)
+				list1->next->prev = list1->prev;
+			list1->next = list1->prev;
+			list1->prev = list1->next->prev;
+			if (list1->prev != NULL)
+				list1->prev->next = list1;
+			else
+				*list = list1;
+			list1->next->prev = list1;
+			print_list(*list);
+		}
+		list1 = temporal;
+	}
 }
