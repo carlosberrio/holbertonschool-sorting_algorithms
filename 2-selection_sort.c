1bfe16970ba5c166768c4d13a18aed505e601dd0
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-20 11:28:23 -0500

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,31 +10,30 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, k = 0;
-	int swap = 0;
+	size_t i, j, k;
+	int swap;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-	    swap = array[i];
-		k = i;
-		for (j = i; j < size; j++)
-		{
-			if (array[j] < swap)
-			{
-				k = j;
-				swap = array[j];
-			}
-		}
-		if (k != i)
-		{
-			array[i] += array[k];
-			array[k] = array[i] - array[k];
-			array[i] -= array[k];
-			print_array(array, size);
-		}
+            swap = array[i];
+            k = i;
+            for (j = i; j < size; j++)
+		    {
+                if (array[j] < swap)
+			    {
+                    k = j;
+                    swap = array[j];
+                }
+            }
+            if (k != i)
+            {
+                array[i] += array[k];
+                array[k] = array[i] - array[k];
+			    array[i] -= array[k];
+			    print_array(array, size);
+		    }
 	}
 }
-
